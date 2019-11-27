fe84c8e6c18b8685d6dc581f5ce46997ff0415b8
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    Change-Id: I3e661f350f9e2ee4e9b65f80b843a08b72e84aa1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5514,6 +5514,11 @@ public final class Settings {
         public static final String DESKTOP_MODE = "desktop_mode";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
